0004d557bdd474e0e32051cfa6090ad9eb8153d8
mouhamadou dia <mouhamadou.dia@medadom.com>
2023-12-21 10:40:53 +0100

    add a test route

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,27 @@ app.get("/Posts/get-all-blog-posts", function(req,res){
         res.json(apiResponse)
     })
 })
+
 app.get("/Posts/get-blog-posts-by-tag", function(req, res) {
     api.getBlogPostsByTag(req.query.tag, function(apiResponse) {
       res.json(apiResponse)
     })
   })
 
-  app.get("/Posts/get-three-newest-posts", function(req, res) {
-    api.getThreeNewestPosts(function(apiResponse) {
-      res.json(apiResponse)
-    })
+app.get("/Posts/get-three-newest-posts", function(req, res) {
+  api.getThreeNewestPosts(function(apiResponse) {
+    res.json(apiResponse)
   })
+})
 
-  app.get("/posts/get-blog-post-by-url-title", function(req, res) {
-    api.getBlogPostByUrlTitle(req.query.urlTitle, function(apiResponse) {
-      res.json(apiResponse)
-    })
+app.get("/posts/get-blog-post-by-url-title", function(req, res) {
+  api.getBlogPostByUrlTitle(req.query.urlTitle, function(apiResponse) {
+    res.json(apiResponse)
   })
+})
+
+app.get("/test", function(req, res) {
+  res.json('success!')
+})
+
 module.exports=app
